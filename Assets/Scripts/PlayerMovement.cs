7ea38586a74bca1a6588681f11b0a8cf22ccbee0
VOSTROA-OFFICE/IceTrooper <trooperice@gmail.com>
2020-12-08 09:50:29 +0100

    added player movement speed variable

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,12 +2,14 @@
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float movementSpeed = 5f;
+
     private void Update()
     {
         Vector2 movement = new Vector2(
             Input.GetAxis("Horizontal"),
             Input.GetAxis("Vertical")
             );
-        transform.Translate(movement * 5f * Time.deltaTime);
+        transform.Translate(movement * movementSpeed * Time.deltaTime);
     }
 }
