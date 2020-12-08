add9435cb4b1135d8f410d247fec1c65c41986ff
VOSTROA-OFFICE/IceTrooper <trooperice@gmail.com>
2020-12-08 09:48:25 +0100

    added player movement; added Player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector2 movement = new Vector2(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical")
+            );
+        transform.Translate(movement * 5f * Time.deltaTime);
+    }
+}
